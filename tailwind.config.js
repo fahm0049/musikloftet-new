4fd8583df193b8900c32b21bfd3b4e017328040f
julieringaas <89411616+julieringaas@users.noreply.github.com>
2022-02-24 14:05:12 +0100

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
   textFillColor: (theme) => theme("borderColor"),
   textStrokeColor: (theme) => theme("borderColor"),
   textStrokeWidth: (theme) => theme("borderWidth"),
-  paintOrder: {
-    fsm: { paintOrder: "fill stroke markers" },
-    fms: { paintOrder: "fill markers stroke" },
-    sfm: { paintOrder: "stroke fill markers" },
-    smf: { paintOrder: "stroke markers fill" },
-    mfs: { paintOrder: "markers fill stroke" },
-    msf: { paintOrder: "markers stroke fill" },
-  },
+  // paintOrder: {
+  //   fsm: { paintOrder: "fill stroke markers" },
+  //   fms: { paintOrder: "fill markers stroke" },
+  //   sfm: { paintOrder: "stroke fill markers" },
+  //   smf: { paintOrder: "stroke markers fill" },
+  //   mfs: { paintOrder: "markers fill stroke" },
+  //   msf: { paintOrder: "markers stroke fill" },
+  // },
 };
