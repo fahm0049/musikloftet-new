907fc78ffb6677e8366c853049ae2aff77298c90
julieringaas <89411616+julieringaas@users.noreply.github.com>
2022-02-24 12:14:43 +0100

    added outline config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,15 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
   plugins: [require("daisyui")],
+  textFillColor: (theme) => theme("borderColor"),
+  textStrokeColor: (theme) => theme("borderColor"),
+  textStrokeWidth: (theme) => theme("borderWidth"),
+  paintOrder: {
+    fsm: { paintOrder: "fill stroke markers" },
+    fms: { paintOrder: "fill markers stroke" },
+    sfm: { paintOrder: "stroke fill markers" },
+    smf: { paintOrder: "stroke markers fill" },
+    mfs: { paintOrder: "markers fill stroke" },
+    msf: { paintOrder: "markers stroke fill" },
+  },
 };
